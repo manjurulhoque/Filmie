f5cc140e6ce55969518b5364c9f706179c9c4a2a
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-09-02 02:44:46 +0600

    home fragment

--- app/src/main/java/google/com/filmie/MainActivity.java
+++ app/src/main/java/google/com/filmie/MainActivity.java
@@ -2,12 +2,26 @@ package google.com.filmie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import google.com.filmie.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    HomeFragment mHomeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        mHomeFragment = (HomeFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_main);
+
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
     }
 }
