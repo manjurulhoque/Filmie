a773f61a6da38b501d5f486641a80da905ff51a1
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-09-04 00:09:45 +0600

    retrofit call for single movie review

--- app/src/main/java/google/com/filmie/networks/MovieService.java
+++ app/src/main/java/google/com/filmie/networks/MovieService.java
@@ -2,6 +2,7 @@ package google.com.filmie.networks;
 
 import google.com.filmie.constants.MovieConstants;
 import google.com.filmie.models.MovieList;
+import google.com.filmie.models.ReviewList;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -11,4 +12,7 @@ public interface MovieService {
     @GET(MovieConstants.GET_MOVIES_URL)
     Call<MovieList> getMovieList(@Query(MovieConstants.API_KEY) String apiKey,
                                  @Query(MovieConstants.SORT_BY) String sortBy, @Query(MovieConstants.PAGE) int page);
+
+    @GET(MovieConstants.GET_REVIEWS_URL)
+    Call<ReviewList> getReviewList(@Query(MovieConstants.API_KEY) String apiKey);
 }
